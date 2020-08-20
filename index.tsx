8947df24fe23dc6eaece69c787738d557d12c25b
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2020-08-21 00:19:51 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -145,13 +145,13 @@ function handleMessage(sender_psid, received_message) {
   } else if(received_message.text.toLowerCase() === "@help"){
     response = {
       "text": `
-        Các lệnh được sử dụng: \n
-        @start: bắt đầu kết nối với bot \n
-        @newgame: bắt đầu một game mới với phòng hiện tại \n
-        @endgame: kết thúc game ngay lập tức \n
-        @role_xx: xx là mã của vai trò, sẽ giúp các bạn biết về vai trò của thẻ \n
-        @role_all: tất cả các vai trò của game \n
-        @help: bộ các lệnh được sử dụng \n
+        Các lệnh được sử dụng:
+        @start: bắt đầu kết nối với bot
+        @newgame: bắt đầu một game mới với phòng hiện tại
+        @endgame: kết thúc game ngay lập tức
+        @role_xx: xx là mã của vai trò, sẽ giúp các bạn biết về vai trò của thẻ
+        @role_all: tất cả các vai trò của game
+        @help: bộ các lệnh được sử dụng
       `
     }
   }else{
