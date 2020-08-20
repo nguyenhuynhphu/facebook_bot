560c86a32316d0b6629fc1690d7c793e81bf483e
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2020-08-20 13:32:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "123"
+    let VERIFY_TOKEN = "EAAJDVVZAcvT0BAAjWSxIooCWPk3M8ZB7t1tTdnxA27wlhoJz3YDr98qA11jfBCWQk8I2p9LvwYDtG6tUisB9rSQr3nshviwb0HLKntcZCv4XoENGscTcgEavKs0er394waPHDOePbIZB5pwZAwzMqZBWGrZAMnxlaEhY0S7LQ2ZBWwZDZD"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
