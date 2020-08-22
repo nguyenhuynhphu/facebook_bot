fe5555649d861e3a8f3511db2cc0d0f9075287f7
nguyenhuynhphu <36849519+nguyenhuynhphu@users.noreply.github.com>
2020-08-22 14:33:44 +0700

    Update Room.ts

--- models/Room.ts
+++ models/Room.ts
@@ -1,19 +1,20 @@
 module.exports = class Room {
-    // number_player: number;
-    // players: Player[];
-    // adminId: string;
-    // // dead_player = [];
-    // // wolves = [];
-    // // villagers = [];
-    // targets = [];
-    // suspects = [];
-    // usingRole = [];
+    number_player: number;
+    players: [];
+    adminId: string;
+    dead_player = [];
+    wolves = [];
+    villagers = [];
+    targets = [];
+    suspects = [];
+    usingRole = [];
+    
     constructor(number_player, players, adminId, targets, suspects, usingRole){
-        number_player = number_player;
-        players = players;
-        adminId = adminId;
-        targets = targets;
-        suspects = suspects;
-        usingRole = usingRole
+        this.number_player = number_player;
+        this.players = players;
+        this.adminId = adminId;
+        this.targets = targets;
+        this.suspects = suspects;
+        this.usingRole = usingRole
     }
 };
